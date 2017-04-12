d567ba28ac78fbc072c070a6006ca61ba293c2f3
Jacob Hartman <junyunharold1@gmail.com>
2017-04-11 22:25:47 -0400

    More frontend work

---
+++ lib/frontends/web/js/components/page-notice.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="page-notice" :class="'ui floating hidden message ' + display_class">
+        <p>{{ message_contents }}</p>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    mounted: function() {
+        $('#page-notice.message').click(function(){
+            $('#page-notice.message').transition('fade up');
+        });
+    },
+    name: "page-notice",
+    props: ['is_visible', 'message_contents', 'display_class'],
+    watch: {
+        is_visible: function(new_value) {
+            var self = this;
+            console.log("notice got", new_value)
+            if (new_value === true) {
+                $('#page-notice.message').transition('fade up');
+            } else {
+                $('#page-notice.message').transition('fade up');
+            }
+        }
+    }
+};
+</script>
